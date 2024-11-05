d30f9ade6da60a5bc2bc9f4f7a963bff1070b9bd
abubakar471 <abubakar.devs@gmail.com>
2024-11-05 14:49:53 +0600

    boilerplate next js 14

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Hero from "@/components/HomePage/Hero/Hero";
 export default function Home() {
   return (
     <main>
-      {/* <Navbar /> */}
+      <Navbar />
       {/* <Hero /> */}
       
     </main>
