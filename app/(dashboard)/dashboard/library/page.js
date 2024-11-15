80b21df576345fd0670e5a5727e276bbfb3c766e
abubakar471 <abubakar.devs@gmail.com>
2024-11-15 22:08:04 +0600

    C:/Program Files/Git/dashboard/library page headline changed

--- app/(dashboard)/dashboard/library/page.js
+++ app/(dashboard)/dashboard/library/page.js
@@ -9,7 +9,7 @@ const VideosPage = async() => {
         <div className=''>
             <h1 className='flex items-center gap-x-2 font-semibold bg-gradient-to-r bg-clip-text from-[#728ee9] via-purple-700 to-[#30a4da] text-transparent text-2xl md:text-3xl lg:text-4xl pt-6 pb-4 border-b border-[#162845]/50 flex-wrap md:flex-nowrap mb-8 '>
                 <MdCollections className='text-[#728ee9] text-3xl lg:text-3xl xl:text-4xl' />
-                Your Videos Collection
+                Your Library
             </h1>
 
             <div>
