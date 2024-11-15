8fd23bcdbcbe490dc7816a9a568fad412a7fec27
abubakar471 <abubakar.devs@gmail.com>
2024-11-16 02:26:02 +0600

    changed design for overview page cards

--- components/DashboardPage/OverviewPage/OverviewStats.jsx
+++ components/DashboardPage/OverviewPage/OverviewStats.jsx
@@ -7,35 +7,37 @@ import { IoIosLink } from 'react-icons/io';
 import { useUser } from '@clerk/nextjs';
 
 const OverviewStats = () => {
-    const {user} = useUser()
+    const { user } = useUser()
     return (
-      user && (
-        <div className='mt-14 flex items-center gap-x-4 gap-y-2 flex-wrap xl:flex-nowrap'>
-        <div className='w-full sm:w-fit px-6 py-4 rounded-2xl bg-gray-500 bg-opacity-5 backdrop-blur-2xl flex items-center gap-x-3 text-neutral-300'>
-            <LuClapperboard className="text-4xl" />
-            <div className='flex flex-col gap-y-0'>
-                <span className='text-sm'>Videos Generated</span>
-                <span className='text-lg'>1 Videos</span>
-            </div>
-        </div>
+        user && (
+            <div className='mt-14 flex items-center gap-x-4 gap-y-2 flex-wrap xl:flex-nowrap'>
+                <div className='w-full sm:w-fit px-6 py-4 rounded-2xl bg-[#FEAEB0] backdrop-blur-2xl flex flex-col sm:flex-row items-center gap-x-3 text-neutral-950'>
+                    <LuClapperboard className="text-4xl" />
 
-        <div className='w-full sm:w-fit px-6 py-4 rounded-2xl bg-gray-500 bg-opacity-5 backdrop-blur-2xl flex items-center gap-x-3 text-neutral-300'>
-            <IoIosLink className="text-4xl" />
+                    <div className='flex flex-col items-center sm:items-start gap-y-0'>
+                        <span className='text-sm'>Videos Generated</span>
+                        <span className='text-lg'>1 Videos</span>
+                    </div>
+                </div>
 
-            <div className='flex flex-col gap-y-0'>
-                <span className='text-sm'>Linked Email Accounts</span>
-                <span className='text-lg'>{user?.emailAddresses[0].emailAddress}</span>
+                <div className='w-full sm:w-fit px-6 py-4 rounded-2xl bg-[#C6D6FE] backdrop-blur-2xl flex flex-col sm:flex-row items-center gap-x-3 text-neutral-950'>
+                    <IoIosLink className="text-4xl" />
+
+                    <div className='flex flex-col items-center sm:items-start gap-y-0'>
+                        <span className='text-sm'>Linked Email Accounts</span>
+                        <span className='text-lg'>{user?.emailAddresses[0].emailAddress}</span>
+                    </div>
+                </div>
+                <div className='w-full sm:w-fit px-6 py-4 rounded-2xl bg-[#EDF58F] backdrop-blur-2xl flex flex-col sm:flex-row items-center gap-x-3 text-neutral-950'>
+                    <DiYii className="text-4xl" />
+
+                    <div className='flex flex-col items-center sm:items-start gap-y-0'>
+                        <span className='text-sm'>Your Plan</span>
+                        <span className='text-lg font-semibold'>Free</span>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div className='w-full sm:w-fit px-6 py-4 rounded-2xl bg-gray-500 bg-opacity-5 backdrop-blur-2xl flex items-center gap-x-3 text-neutral-300'>
-            <DiYii className="text-4xl" />
-            <div className='flex flex-col gap-y-0'>
-                <span className='text-sm'>Your Plan</span>
-                <span className='text-lg text-[#1aadf1]'>Free</span>
-            </div>
-        </div>
-    </div>
-      )
+        )
     )
 }
 
