848ca55fb4091ea51e8e21685b48b5b8b3c19255
abubakar471 <abubakar.devs@gmail.com>
2024-11-16 15:16:52 +0600

    overview page apos error fixed

--- components/DashboardPage/OverviewPage/OverviewHeading.jsx
+++ components/DashboardPage/OverviewPage/OverviewHeading.jsx
@@ -40,7 +40,7 @@ const OverviewHeading = () => {
                 localStatus === "MORNING" && (`Good Morning! Let's kickstart your day 🌅`)
               }
               {
-                localStatus === "AFTERNOON" && (`Good Afternoon! Here' what's happening 🕶️`)
+                localStatus === "AFTERNOON" && (`Good Afternoon! Here's what's happening 🕶️`)
               }
               {
                 localStatus === "EVENING" && (`Evening Overview: Here's what's left 📋`)
