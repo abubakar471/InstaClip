23df03dd689eb8be8c13573e835302e90700d18a
abubakar471 <abubakar.devs@gmail.com>
2024-11-27 19:44:29 +0600

    enabled use cache next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@ import { withNextVideo } from "next-video/process";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["cdn.bhlist.co.in", "images.clerk.dev", "cdn.discordapp.com", "images.unsplash.com","localhost"],
+        domains: ["cdn.bhlist.co.in", "images.clerk.dev", "cdn.discordapp.com", "images.unsplash.com", "localhost"],
+    },
+    experimental: {
+        dynamicIO: true,
     },
 };
 
