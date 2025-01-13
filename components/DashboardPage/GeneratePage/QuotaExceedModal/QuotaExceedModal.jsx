a3a2a27d7889c90224de33cc6fe180b33840e468
abubakar471 <abubakar.devs@gmail.com>
2025-01-13 17:42:14 +0600

    added daily reset of quota

---
+++ components/DashboardPage/GeneratePage/QuotaExceedModal/QuotaExceedModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const QuotaExceedModal = ({ open, setOpen }) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default QuotaExceedModal
